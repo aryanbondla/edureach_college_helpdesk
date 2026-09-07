2ccf6dfc0b0a69fcce8f36b04a0749411cc019b8
aryanbondla <bondlaaryan@gmail.com>
2026-09-07 11:05:19 +0530

    installing ci/cd and automating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,8 @@ export default function App() {
 
       {/* Floating chat button — visible on all pages */}
       <FloatingChatButton />
+      <h1>Welcome to the App!
+      </h1>
     </>
   );
 }
